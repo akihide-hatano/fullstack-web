e257897adaf60619479afec1dd5679ebbfb24880
akihide-hatano <a.h.19841202@gmail.com>
2025-04-19 17:31:04 +0900

    050/07クラスを作成しました。

--- 050_PHP基礎Part3/0700_クラス/start/index.php
+++ 050_PHP基礎Part3/0700_クラス/start/index.php
@@ -1,10 +1,19 @@
 <?php 
 class Person
 {
-    private $name;
+    public $name;
     public $age;
+
+    function __construct($name,$age)
+    {
+        $this -> name = $name;
+        $this -> age = $age;
+    }
 
     function hello() {
         echo 'hello, ' . $this->name;
     }
 }
+
+$bob = new Person("bob",18);
+echo $bob->hello();
