b908dc496fa2a420caa2c9f63df4cfef4e42ca8c
akihide-hatano <a.h.19841202@gmail.com>
2025-04-21 07:58:21 +0900

    取り上げず終わりました。

--- 050_PHP基礎Part3/1300_理解度チェック(クラス)/start/index.php
+++ 050_PHP基礎Part3/1300_理解度チェック(クラス)/start/index.php
@@ -7,33 +7,33 @@
  * ヒント）PHP_EOL: 改行するための特殊な定数です。
  */
 
-class MyFileWriter{
-    private $filename;
-    private $content = "";
-    public const APPEND = FILE_APPEND; // public const で定数として定義
+ class MyFileWriter{
 
-    function __construct($filename)
+    public $filename;
+    public $content = "";
+    public $APPEND = FILE_APPEND;
+
+    function __construct( $filename)
     {
         $this -> filename = $filename;
     }
 
     function append($content){
-        $this -> content = $this -> content . $content;
+        $this -> content = $this ->content . $content;
         return $this;
     }
 
     function newline(){
-        $this -> content = $this -> content . PHP_EOL;
+        $this -> content =  $this -> content .PHP_EOL;
         return $this;
     }
 
-    function commit( $flag = 0) {
-        file_put_contents( $this->filename, $this->content , $flag ?? 0); // デフォルト値を null 合体演算子で処理
-        $this -> content = "";
+    function commit(int $flag = 0){
+        file_put_contents($this->filename, $this->content, $flag);
         return $this;
-    }    
+    }
 
-}
+ }
 
 // $content = 'Hello, Bob.'; // append
 // $content .= PHP_EOL; // newline
@@ -60,5 +60,5 @@ $file->append('Hello, Bob.')
     ->newline()
     ->commit()
     ->append('Good night, Bob.')
-    ->commit(MyFileWriter::APPEND);
+    ->commit($file->APPEND);
 
